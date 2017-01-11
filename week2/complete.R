2d9655ce70c8f5e5c97d255e19ae7bf7fdf2d229
Chuan Lee <chuanleerc@gmail.com>
2017-01-11 21:50:33 +0000

    Update complete.R

--- week2/complete.R
+++ week2/complete.R
@@ -1,3 +1,10 @@
+# Chuan Lee
+# R Programming course by Johns Hopkins University
+# Coursera https://www.coursera.org/learn/r-programming/
+# January 2017 Session
+# Programming assignment 1: Air Pollution
+# Week 2 - Part 2
+
 # Write a function that reads a directory full of files and reports the number
 # of completely observed cases in each data file. The function should return a
 # data frame where the first column is the name of the file and the second
